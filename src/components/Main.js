2b8984fb9801226fc980aad9fd75bba1c5fcd039
Riya Shah <46188142+shahriya1995@users.noreply.github.com>
2026-09-08 15:48:01 -0700

    Update resume link in Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -13,7 +13,7 @@ function Main() {
                     <h1>i'm riya shah</h1>
                     <h3>Software Developer</h3>
                     {/*<a href="/contact" className="btn hero-btn">hire me</a>*/}
-                    <a className="btn center-btn" href="https://drive.google.com/file/d/1c8l1ejVGeDSFQS0yJeiZk5qYEtvpAMm4/view?usp=sharing">RESUME</a>
+                    <a className="btn center-btn" href="https://drive.google.com/file/d/1pob82xHRTxzr-oENq6wQAE6aFdInkXQ4/view?usp=sharing">RESUME</a>
 
                     <br />
                     <br />
